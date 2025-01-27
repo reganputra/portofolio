d0247bcecfcc038c1c478758ba62b588b6ef15aa
reganputra <ramadhanregan334@gmail.com>
2025-01-27 09:43:13 +0700

    feat: add reveal js

--- JS/script.js
+++ JS/script.js
@@ -30,3 +30,12 @@ window.onscroll = () => {
     menuIcon.classList.remove('fa-xmark');
     navbar.classList.remove('active');
 };
+
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, heading', { origin: 'top',});
+ScrollReveal().reveal('.home-img, .skill-container, .project-box', { origin: 'bottom' });
